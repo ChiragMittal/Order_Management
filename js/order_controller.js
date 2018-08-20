7f67c425f198482ae1b2e30106177e843271d8ef
Chirag Mittal <chiragmittal97@yahoo.co.in>
2018-08-19 17:49:01 -0700

    card and list done

---
+++ js/order_controller.js
@@ -0,0 +1,98 @@
+var app = angular.module('order', []);
+
+app.controller('customer', function($scope,$timeout){
+
+    $scope.customers = [
+        {
+          name: "Fred Flintstone",
+          city: "Bedrock",
+          state: "Washingstone",
+          gender: "Male",
+          orderCount : 4
+        },
+        {
+          name: "John Smith",
+          city: "Norfolk",
+          state: "Virginia",
+          gender: "Male",
+          orderCount : 6
+        },
+        {
+          name: "Frank Livingston",
+          city: "Pittsburgh",
+          state: "Pennsylvania",
+          gender: "Male",
+          orderCount : 1
+        },
+        {
+          name: "Judy Green",
+          city: "Cincinatti",
+          state: "Ohio",
+          gender: "Female",
+          orderCount : 7
+        },
+        {
+          name: "Pat Thomas",
+          city: "New York",
+          state: "New York",
+          gender: "Male",
+          orderCount : 9
+        },
+        {
+          name: "Betty Rubble",
+          city: "Bedrock",
+          state: "Washingstone",
+          gender: "Female",
+          orderCount : 3
+        },
+        {
+          name: "Martha Smith",
+          city: "Norfolk",
+          state: "Virginia",
+          gender: "Female",
+          orderCount : 10
+        },
+        {
+          name: "Liz Livingston",
+          city: "Pittsburgh",
+          state: "Pennsylvania",
+          gender: "Female",
+          orderCount : 8
+        },
+        {
+          name: "Howard McGovern",
+          city: "Denver",
+          state: "Colorado",
+          gender: "Male",
+          orderCount : 5
+        }
+    ]; 
+
+    $scope.cardView = true;
+	$scope.listView = false;
+	
+	
+	
+	
+	$scope.cardViewClick =  function() {
+		$timeout(function() {
+			$scope.cardView = true;
+		}, 500);
+			$scope.listView = false;
+	}
+	$scope.listViewClick =  function() {
+			$scope.cardView = false;
+    $timeout(function() {
+			$scope.listView = true;		
+		}, 500);
+    }
+    // $scope.viewMore = function() {
+	// 	$('.card-view-info').addClass('active');
+	// }
+	
+	// $scope.close = function() {
+	// 	$('.card-view-info').removeClass('active');
+	// }
+
+}
+);
