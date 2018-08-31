ce0c0cd3fadc3977beb57631d9729076b6de83bb
Chirag Mittal <chiragmittal97@yahoo.co.in>
2018-08-31 14:34:58 -0700

    adding orders into customers almost done

--- server.js
+++ server.js
@@ -5,9 +5,21 @@ var app        = express();
 var bodyParser = require('body-parser');
 var MongoClient = require("mongodb").MongoClient;
 var mongoose = require("mongoose");
-var shortid = require("shortid");
+var assert = require("assert");
 
 // console.log(shortid.generate('1234567890'));
+
+// var Orders = mongoose.model('Orders',{
+//     orders:[
+//         {
+      
+//             o_name : String ,
+//             quantity : Number ,
+//             price : Number 
+//         }
+//     ]
+    
+// });
 
 var Customers = mongoose.model('Customers',{
     id: {type:String , required:true ,ref: 'Orders'} ,
@@ -22,33 +34,24 @@ var Customers = mongoose.model('Customers',{
     // window : {
 	// 	title : String
     // },
-    // orders :[
-    //     {
-    //         o_id : Number ,
-    //         o_name : String ,
-    //         quantity : Number ,
-    //         price : Number 
-    //     }
-    // ]
-});
-
-var Orders = mongoose.model('Orders',{
-    orders:[
+    orders :[
         {
-            o_name : { type:String ,ref: 'Customers'} ,
+            
+            o_name : String ,
             quantity : Number ,
             price : Number 
         }
     ]
-    
 });
 
-function getUserWithPosts(id){
-    return Customers.findOne({ id: id })
-      .populate('Orders').exec((err, orders) => {
-        console.log("Populated User " + orders);
-      })
-  }
+
+
+// function getUserWithPosts(id){
+//     return Customers.findOne({ id: id })
+//       .populate('Orders').exec((err, orders) => {
+//         console.log("Populated User " + orders);
+//       })
+//   }
 
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
@@ -78,18 +81,20 @@ mongoose.connect("mongodb://localhost:27017/customers",(err,db) => {
     }
 });
 
+mongoose.connect("mongodb://localhost:27017/orders",(err,db) => {
+    if(!err){
+        console.log("hurray");
+
+    
+    }
+});
+
 app.post('/new_info',(req,res)=>{
-    console.log(req.body);
+    console.log(req.body.name);
     // res.json(req.body);
 
     // console.log(res.send(req.body))
 var user = new Customers(req.body);
-//         // user.id = req.body.id;
-//         // user.name = req.body.name;
-//         // user.city = req.body.city;
-//         // user.state = req.body.state;
-//         // user.gender = req.body.gender;
-
         console.log(user)
 
         user.save((err, doc) => {
@@ -130,11 +135,48 @@ app.get("/customer/:id", (req, res) => {
 
    });
 
+   app.post("/customer/order_list/:id",(req,res) => {
+
+    var new_order = new Customers({
+
+   id: String(req.params.id),
+    orders :[
+            {
+                "o_name" : "Baseball  Bat",
+                "quantity" : 3,
+                "price" : 10
+            }
+        
+    ]
+
+    });
+
+    var user = String(req.params.id);
+    console.log(new_order.orders)
+
+        Customers.findOneAndUpdate({id:user},{ $push: { orders : new_order.orders }  }, (err, items) => {
+            if (err) res.status(500).send(err)
+        
+           
+                        res.status(200).send(items);
+                    
+            
+                })
+       
+    
+      
+    
+
+
+   });
+
    app.get("/customer/order_list/:id", (req, res) => {
   
     console.log(req.params.id);
     var user = String(req.params.id);
-    console.log(user)
+    console.log(user);
+
+    
 
     Customers.find({id:user}, (err, items) => {
         if (err) res.status(500).send(err)
